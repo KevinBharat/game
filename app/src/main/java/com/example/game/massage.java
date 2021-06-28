f3c4d03d65a68b7954f8b931eb0888754a3663e3
KevinBharat <57409444+KevinBharat@users.noreply.github.com>
2021-06-28 04:15:21 -0400

    game

---
+++ app/src/main/java/com/example/game/massage.java
@@ -0,0 +1,15 @@
+package com.example.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class massage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_massage);
+        setTitle("Massage");
+
+    }
+}
